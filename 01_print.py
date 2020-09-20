29822e3111f32b301342c18c0b03c7cc4a0100ea
k1610221 <69496277+k1610221@users.noreply.github.com>
2020-09-20 23:32:53 +0900

    20.09.20
    다른 옵션: file, flush

---
+++ 01_print.py
@@ -0,0 +1,7 @@
+print("Hello, world!")
+print("Hello, ",end="world!\n") #end의 디폴트값:'\n'
+print("Hello","world",sep=", ") #sep의 디폴트값:' '
+
+var1="Hello"
+var2="world"
+print(var1,var2, sep=", ",end="!\n")
